2d7685005b7d9c75793a360a73617aae2dd53445
Andrzej Soltysik <adzi11235@gmail.com>
2022-03-22 19:01:57 +0100

    Add AvlTree data structure

---
+++ src/Container/AvlTree/AvlTree.cpp
@@ -0,0 +1,701 @@
+#include "Container/AvlTree/AvlTree.h"
+
+#include "Utils/Utils.h"
+
+AvlTree::AvlTree(std::initializer_list<DataType> initList)
+{
+    for (const auto& item: initList)
+    {
+        Insert(item);
+    }
+}
+
+AvlTree::AvlTree(const AvlTree& rhs)
+{
+    SetRoot(CopySubtree(rhs, rhs.Root()));
+    size = rhs.size;
+}
+
+AvlTree::AvlTree(AvlTree&& rhs) noexcept
+{
+    NIL = rhs.NIL;
+    size = rhs.size;
+
+    rhs.NIL = nullptr;
+    rhs.size = 0u;
+}
+
+AvlTree& AvlTree::operator=(const AvlTree& rhs)
+{
+    if (this == &rhs)
+    {
+        return *this;
+    }
+    Clear();
+    SetRoot(CopySubtree(rhs, rhs.Root()));
+    size = rhs.size;
+
+    return *this;
+}
+
+AvlTree& AvlTree::operator=(AvlTree&& rhs) noexcept
+{
+    Clear();
+    NIL = rhs.NIL;
+    size = rhs.size;
+
+    rhs.NIL = nullptr;
+    rhs.size = 0u;
+
+    return *this;
+}
+
+AvlTree::~AvlTree()
+{
+    Clear();
+    delete NIL;
+    NIL = nullptr;
+}
+
+AvlTree::Iterator AvlTree::Insert(DataType value)
+{
+    Node* newNode = MakeNode(value);
+    Node* parent = NIL;
+    Node* iterator = Root();
+
+    size++;
+
+    while (iterator != NIL)
+    {
+        parent = iterator;
+
+        if (value < iterator->value)
+        {
+            iterator = iterator->left;
+        }
+        else
+        {
+            iterator = iterator->right;
+        }
+    }
+    if (value > parent->value)
+    {
+        parent->right = newNode;
+    }
+    else
+    {
+        parent->left = newNode;
+    }
+    newNode->parent = parent;
+
+    InsertFix(newNode);
+
+    return {this, newNode};
+}
+
+bool AvlTree::Remove(DataType value)
+{
+    Node* nodeToRemove = Find(value, Root());
+    if (nodeToRemove == NIL)
+    {
+        return false;
+    }
+
+    size--;
+
+    Node* successor;
+    if (nodeToRemove->left == NIL && nodeToRemove->right == NIL)
+    {
+        successor = RemoveNodeWithoutChildren(nodeToRemove);
+    }
+
+    else if (nodeToRemove->left == NIL || nodeToRemove->right == NIL)
+    {
+        successor = RemoveNodeWithOneChild(nodeToRemove);
+    }
+
+    else
+    {
+        successor = RemoveNodeWithTwoChildren(nodeToRemove);
+    }
+
+    RemoveFix(successor);
+
+    return true;
+}
+
+void AvlTree::Clear()
+{
+    if (NIL != nullptr)
+    {
+        if (Root() != nullptr)
+        {
+            RemoveSubtree(Root());
+
+        }
+        NIL->left = NIL;
+    }
+}
+
+AvlTree::ConstIterator AvlTree::Find(DataType value) const
+{
+    return {this, Find(value, Root())};
+}
+
+AvlTree::Iterator AvlTree::Find(DataType value)
+{
+    return {this, Find(value, Root())};
+}
+
+AvlTree::DataType AvlTree::Min() const
+{
+    return Min(Root())->value;
+}
+
+AvlTree::DataType AvlTree::Max() const
+{
+    return Max(Root())->value;
+}
+
+size_t AvlTree::Size() const noexcept
+{
+    return size;
+}
+
+AvlTree::Iterator AvlTree::begin() noexcept
+{
+    return {this, Min(Root())};
+}
+
+AvlTree::Iterator AvlTree::end() noexcept
+{
+    return {this, NIL};
+}
+
+AvlTree::ConstIterator AvlTree::begin() const noexcept
+{
+    return {this, Min(Root())};
+}
+
+AvlTree::ConstIterator AvlTree::end() const noexcept
+{
+    return {this, NIL};
+}
+
+AvlTree::ConstIterator AvlTree::cbegin() const noexcept
+{
+    return {this, Min(Root())};
+}
+
+AvlTree::ConstIterator AvlTree::cend() const noexcept
+{
+    return {this, NIL};
+}
+
+std::string AvlTree::ToString() const
+{
+    std::string result;
+    ToString(result, "", Root(), false);
+    return result;
+}
+
+std::ostream& operator<<(std::ostream& os, const AvlTree& tree)
+{
+    if (!os.good())
+    {
+        return os;
+    }
+    tree.Serialize(os, tree.Root());
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, AvlTree& tree)
+{
+    if (!is.good())
+    {
+        return is;
+    }
+    tree.SetRoot(tree.Deserialize(is, tree.NIL));
+    return is;
+}
+
+AvlTree::Height AvlTree::Node::BalanceFactor() const noexcept
+{
+    return static_cast<Height>(left->height - right->height);
+}
+
+void AvlTree::Node::UpdateHeight() noexcept
+{
+    height = static_cast<Height>(1 + Utils::Max(left->height, right->height));
+}
+
+AvlTree::Node* AvlTree::LeftRotate(Node* node) noexcept
+{
+    Node* child = node->right;
+    Node* grandChild = child->left;
+
+    child->left = node;
+    child->parent = node->parent;
+
+    if (node->parent->left == node)
+    {
+        child->parent->left = child;
+    }
+    else
+    {
+        child->parent->right = child;
+    }
+
+    node->right = grandChild;
+    node->parent = child;
+    grandChild->parent = node;
+
+    node->UpdateHeight();
+    child->UpdateHeight();
+
+    return child;
+}
+
+AvlTree::Node* AvlTree::RightRotate(Node* node) noexcept
+{
+    Node* child = node->left;
+    Node* grandChild = child->right;
+
+    child->right = node;
+    child->parent = node->parent;
+    if (node->parent->left == node)
+    {
+        child->parent->left = child;
+    }
+    else
+    {
+        child->parent->right = child;
+    }
+    node->left = grandChild;
+    grandChild->parent = node;
+    node->parent = child;
+
+    node->UpdateHeight();
+    child->UpdateHeight();
+
+    return child;
+}
+
+AvlTree::Node* AvlTree::MakeNil()
+{
+    Node* nil = new Node{};
+
+    nil->height = 0;
+    nil->parent = nil;
+    nil->left = nil;
+    nil->right = nil;
+
+    return nil;
+}
+
+AvlTree::Node* AvlTree::MakeNode(DataType value) const
+{
+    Node* node = new Node{};
+    node->value = value;
+    node->left = NIL;
+    node->right = NIL;
+    node->height = 1;
+
+    return node;
+}
+
+void AvlTree::SetRoot(AvlTree::Node* node) const noexcept
+{
+    NIL->left = node;
+    node->parent = NIL;
+}
+
+AvlTree::Node* AvlTree::Root() const noexcept
+{
+    return NIL->left;
+}
+
+AvlTree::Node* AvlTree::Min(AvlTree::Node* node) const noexcept
+{
+    while (node->left != NIL)
+    {
+        node = node->left;
+    }
+    return node;
+}
+
+AvlTree::Node* AvlTree::Max(AvlTree::Node* node) const noexcept
+{
+    while (node->right != NIL)
+    {
+        node = node->right;
+    }
+    return node;
+}
+
+AvlTree::Node* AvlTree::Find(const DataType& value, Node* root) const noexcept
+{
+    while (root != NIL)
+    {
+        if (value == root->value)
+        {
+            return root;
+        }
+        if (value > root->value)
+        {
+            root = root->right;
+        }
+        else
+        {
+            root = root->left;
+        }
+    }
+    return NIL;
+}
+
+void AvlTree::InsertFix(Node* node) const noexcept
+{
+    Node* parent = node->parent;
+    Node* child = node;
+
+    while (parent != NIL)
+    {
+        parent->UpdateHeight();
+        Height balance = parent->BalanceFactor();
+
+        if (balance > 1)
+        {
+            if (node->value < parent->left->value)
+            {
+                parent = RightRotate(parent);
+            }
+            else
+            {
+                parent->left = LeftRotate(parent->left);
+                parent->left->parent = parent;
+            }
+        }
+        else if (balance < -1)
+        {
+            if (node->value < parent->right->value)
+            {
+                parent->right = RightRotate(parent->right);
+                parent->right->parent = parent;
+            }
+            else
+            {
+                parent = LeftRotate(parent);
+            }
+        }
+
+        child = parent;
+        parent = parent->parent;
+    }
+    SetRoot(child);
+}
+
+void AvlTree::RemoveFix(Node* node) const noexcept
+{
+    Node* child = Root();
+
+    while (node != NIL)
+    {
+        node->UpdateHeight();
+        Height balance = node->BalanceFactor();
+
+        if (balance > 1)
+        {
+            if (node->left->BalanceFactor() >= 0)
+            {
+                node = RightRotate(node);
+            }
+            else
+            {
+                node->left = LeftRotate(node->left);
+                node = RightRotate(node);
+            }
+        }
+        else if (balance < -1)
+        {
+            if (node->right->BalanceFactor() <= 0)
+            {
+                node = LeftRotate(node);
+            }
+            else
+            {
+                node->right = RightRotate(node->right);
+                node = LeftRotate(node);
+            }
+        }
+
+        child = node;
+        node = node->parent;
+    }
+    SetRoot(child);
+}
+
+void AvlTree::RemoveSubtree(AvlTree::Node* root)
+{
+    if (root == NIL)
+    {
+        return;
+    }
+
+    RemoveSubtree(root->left);
+    RemoveSubtree(root->right);
+
+    delete root;
+    root = nullptr;
+    size--;
+}
+
+AvlTree::Node* AvlTree::CopySubtree(const AvlTree& tree, AvlTree::Node* root)
+{
+    if (root == tree.NIL)
+    {
+        return NIL;
+    }
+
+    Node* newRoot = new Node;
+    newRoot->value = root->value;
+    newRoot->height = root->height;
+    newRoot->left = CopySubtree(tree, root->left);
+    newRoot->left->parent = newRoot;
+
+    newRoot->right = CopySubtree(tree, root->right);
+    newRoot->right->parent = newRoot;
+    return newRoot;
+}
+
+AvlTree::Node* AvlTree::RemoveNodeWithoutChildren(Node* nodeToRemove) const
+{
+    Node* successor = nodeToRemove->parent;
+
+    if (nodeToRemove->parent->left == nodeToRemove)
+    {
+        nodeToRemove->parent->left = NIL;
+    }
+    else
+    {
+        nodeToRemove->parent->right = NIL;
+    }
+
+    delete nodeToRemove;
+    nodeToRemove = nullptr;
+
+    return successor;
+}
+
+AvlTree::Node* AvlTree::RemoveNodeWithOneChild(Node* nodeToRemove) const
+{
+    Node* successor = nodeToRemove->left != NIL ? nodeToRemove->left : nodeToRemove->right;
+    successor->parent = nodeToRemove->parent;
+
+    if (nodeToRemove->parent->left == nodeToRemove)
+    {
+        successor->parent->left = successor;
+    }
+    else
+    {
+        successor->parent->right = successor;
+    }
+
+    delete nodeToRemove;
+    nodeToRemove = nullptr;
+
+    return successor;
+}
+
+AvlTree::Node* AvlTree::RemoveNodeWithTwoChildren(Node* node) const
+{
+    Node* successor = Max(node->left);
+    Utils::Swap(successor->value, node->value);
+
+    if (successor->left == NIL && successor->right == NIL)
+    {
+        successor = RemoveNodeWithoutChildren(successor);
+    }
+
+    else if (successor->left == NIL || successor->right == NIL)
+    {
+        successor = RemoveNodeWithOneChild(successor);
+    }
+    return successor;
+}
+
+void AvlTree::ToString(std::string& result, const std::string& prefix, const Node* node, bool isRight) const
+{
+    if (node != NIL)
+    {
+        result += prefix;
+
+        result += (isRight ? Utils::VERTICAL_BAR_RIGHT : Utils::HALF_VERTICAL_BAR_RIGHT);
+        result += Utils::HORIZONTAL_BAR;
+
+        result += Utils::Parser::NumberToString(node->value) + "\n";
+
+        ToString(result, prefix + (isRight ? Utils::VERTICAL_BAR : " ") + " ", node->right, true);
+        ToString(result, prefix + (isRight ? Utils::VERTICAL_BAR : " ") + " ", node->left, false);
+    }
+}
+
+void AvlTree::Serialize(std::ostream& os, AvlTree::Node* node) const
+{
+    if (!os.good())
+    {
+        return;
+    }
+    if (node != NIL)
+    {
+        os << NIL_VALUE << " ";
+    }
+    else
+    {
+        os << node->value << " ";
+        Serialize(os, node->left);
+        Serialize(os, node->right);
+    }
+}
+
+AvlTree::Node* AvlTree::Deserialize(std::istream& is, AvlTree::Node* node)
+{
+    if (!is.good())
+    {
+        return NIL;
+    }
+    int64_t value;
+    is >> value;
+    if (value == NIL_VALUE)
+    {
+        return NIL;
+    }
+    Node* newNode = MakeNode(static_cast<DataType>(value));
+
+    newNode->parent = node;
+    newNode->left = Deserialize(is, newNode);
+    newNode->right = Deserialize(is, newNode);
+
+    return newNode;
+}
+
+AvlTreeConstIterator::AvlTreeConstIterator(const AvlTree* avlTree, AvlTree::Node* node) noexcept
+    : avlTree(avlTree)
+    , node(node)
+{ }
+
+AvlTreeConstIterator::reference AvlTreeConstIterator::operator*() const noexcept
+{
+    return node->value;
+}
+
+AvlTreeConstIterator::pointer AvlTreeConstIterator::operator->() const noexcept
+{
+    return &(**this);
+}
+
+AvlTreeConstIterator& AvlTreeConstIterator::operator++() noexcept
+{
+    if (node->right == avlTree->NIL)
+    {
+        AvlTree::Node* parent;
+        while ((parent = node->parent) != avlTree->NIL && node == parent->right)
+        {
+            node = parent;
+        }
+        node = parent;
+    }
+    else
+    {
+        node = avlTree->Min(node->right);
+    }
+
+    return *this;
+}
+
+AvlTreeConstIterator AvlTreeConstIterator::operator++(int) noexcept
+{
+    AvlTreeConstIterator tmp = *this;
+    ++(*this);
+    return tmp;
+}
+
+AvlTreeConstIterator& AvlTreeConstIterator::operator--() noexcept
+{
+    if (node == avlTree->NIL)
+    {
+        node = avlTree->Max(avlTree->Root());
+    }
+    else if (node->left == avlTree->NIL)
+    {
+        AvlTree::Node* parent;
+        while ((parent = node->parent) != avlTree->NIL && node == parent->left)
+        {
+            node = parent;
+        }
+        if (node != avlTree->NIL)
+        {
+            node = parent;
+        }
+    }
+    else
+    {
+        node = avlTree->Max(node->left);
+    }
+
+    return *this;
+}
+
+AvlTreeConstIterator AvlTreeConstIterator::operator--(int) noexcept
+{
+    AvlTreeConstIterator tmp = *this;
+    --(*this);
+    return tmp;
+}
+
+bool AvlTreeConstIterator::operator==(const AvlTreeConstIterator& rhs) const noexcept
+{
+    return node == rhs.node;
+}
+
+bool AvlTreeConstIterator::operator!=(const AvlTreeConstIterator& rhs) const noexcept
+{
+    return !(*this == rhs);
+}
+
+AvlTreeIterator::reference AvlTreeIterator::operator*() const noexcept
+{
+    return const_cast<reference>(AvlTreeConstIterator::operator*());
+}
+
+AvlTreeIterator::pointer AvlTreeIterator::operator->() const noexcept
+{
+    return &(**this);
+}
+
+AvlTreeIterator& AvlTreeIterator::operator++() noexcept
+{
+    AvlTreeConstIterator::operator++();
+    return *this;
+}
+
+AvlTreeIterator AvlTreeIterator::operator++(int) noexcept
+{
+    AvlTreeIterator tmp = *this;
+    AvlTreeConstIterator::operator++();
+    return tmp;
+}
+
+AvlTreeIterator& AvlTreeIterator::operator--() noexcept
+{
+    AvlTreeConstIterator::operator--();
+    return *this;
+}
+
+AvlTreeIterator AvlTreeIterator::operator--(int) noexcept
+{
+    AvlTreeIterator tmp = *this;
+    AvlTreeConstIterator::operator--();
+    return tmp;
+}
+
+
---
+++ src/Container/AvlTree/AvlTree.h
@@ -0,0 +1,142 @@
+#pragma once
+
+#include <istream>
+
+struct AvlTreeIterator;
+struct AvlTreeConstIterator;
+
+class AvlTree
+{
+public:
+    friend struct AvlTreeIterator;
+    friend struct AvlTreeConstIterator;
+
+    using DataType = int32_t;
+    using Iterator = AvlTreeIterator;
+    using ConstIterator = AvlTreeConstIterator;
+
+    [[nodiscard]] static constexpr const char* ClassName() { return "AvlTree"; }
+
+    AvlTree() = default;
+    AvlTree(std::initializer_list<DataType> initList);
+    AvlTree(const AvlTree& rhs);
+    AvlTree(AvlTree&& rhs) noexcept;
+    AvlTree& operator=(const AvlTree& rhs);
+    AvlTree& operator=(AvlTree&& rhs) noexcept;
+    ~AvlTree();
+
+    Iterator Insert(DataType value);
+    bool Remove(DataType value);
+    void Clear();
+
+    [[nodiscard]] ConstIterator Find(DataType value) const;
+    [[nodiscard]] Iterator Find(DataType value);
+
+    [[nodiscard]] DataType Min() const;
+    [[nodiscard]] DataType Max() const;
+
+    [[nodiscard]] size_t Size() const noexcept;
+
+    [[nodiscard]] Iterator begin() noexcept;
+    [[nodiscard]] Iterator end() noexcept;
+    [[nodiscard]] ConstIterator begin() const noexcept;
+    [[nodiscard]] ConstIterator end() const noexcept;
+    [[nodiscard]] ConstIterator cbegin() const noexcept;
+    [[nodiscard]] ConstIterator cend() const noexcept;
+
+    [[nodiscard]] std::string ToString() const;
+    friend std::ostream& operator<<(std::ostream& os, const AvlTree& tree);
+    friend std::istream& operator>>(std::istream& is, AvlTree& tree);
+private:
+    using Height = int8_t;
+    struct Node
+    {
+        Height height;
+        DataType value;
+
+        Node* left;
+        Node* right;
+        Node* parent;
+
+        [[nodiscard]] Height BalanceFactor() const noexcept;
+        void UpdateHeight() noexcept;
+    };
+
+    inline static constexpr int64_t NIL_VALUE = -1;
+
+    static Node* LeftRotate(Node* node) noexcept;
+    static Node* RightRotate(Node* node) noexcept;
+
+    [[nodiscard]] static Node* MakeNil();
+    [[nodiscard]] Node* MakeNode(DataType value) const;
+
+    void SetRoot(Node* node) const noexcept;
+    [[nodiscard]] Node* Root() const noexcept;
+
+    [[nodiscard]] Node* Min(Node* node) const noexcept;
+    [[nodiscard]] Node* Max(Node* node) const noexcept;
+    [[nodiscard]] Node* Find(const DataType& value, Node* root) const noexcept;
+
+    void InsertFix(Node* node) const noexcept;
+    void RemoveFix(Node* node) const noexcept;
+
+    void RemoveSubtree(Node* root);
+    [[nodiscard]] Node* CopySubtree(const AvlTree& tree, Node* root);
+
+    Node* RemoveNodeWithoutChildren(Node* node) const;
+    Node* RemoveNodeWithOneChild(Node* node) const;
+    Node* RemoveNodeWithTwoChildren(Node* node) const;
+
+    void ToString(std::string& result, const std::string& prefix, const Node* node, bool isRight) const;
+    void Serialize(std::ostream& os, Node* node) const;
+    [[nodiscard]] Node* Deserialize(std::istream& is, Node* node);
+
+    Node* NIL = MakeNil();
+    size_t size = 0;
+};
+
+struct AvlTreeConstIterator
+{
+    using iterator_category = std::bidirectional_iterator_tag;
+    using value_type = AvlTree::DataType;
+    using pointer = const value_type*;
+    using reference = const value_type&;
+
+    AvlTreeConstIterator() = default;
+    AvlTreeConstIterator(const AvlTree* avlTree, AvlTree::Node* node) noexcept;
+
+    [[nodiscard]] reference operator*() const noexcept;
+
+    [[nodiscard]] pointer operator->() const noexcept;
+
+    AvlTreeConstIterator& operator++() noexcept;
+    AvlTreeConstIterator operator++(int) noexcept;
+    AvlTreeConstIterator& operator--() noexcept;
+    AvlTreeConstIterator operator--(int) noexcept;
+
+    [[nodiscard]] bool operator==(const AvlTreeConstIterator& rhs) const noexcept;
+    [[nodiscard]] bool operator!=(const AvlTreeConstIterator& rhs) const noexcept;
+
+protected:
+    const AvlTree* avlTree;
+    AvlTree::Node* node;
+};
+
+struct AvlTreeIterator : public AvlTreeConstIterator
+{
+    using iterator_category = std::bidirectional_iterator_tag;
+    using value_type = AvlTree::DataType;
+    using pointer = value_type*;
+    using reference = value_type&;
+
+    using AvlTreeConstIterator::AvlTreeConstIterator;
+
+    [[nodiscard]] reference operator*() const noexcept;
+
+    [[nodiscard]] pointer operator->() const noexcept;
+
+    AvlTreeIterator& operator++() noexcept;
+    AvlTreeIterator operator++(int) noexcept;
+    AvlTreeIterator& operator--() noexcept;
+    AvlTreeIterator operator--(int) noexcept;
+};

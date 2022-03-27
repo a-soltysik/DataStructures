5f374486c1783eedd0999295fe31a897a4afd391
Andrzej Soltysik <adzi11235@gmail.com>
2022-03-27 17:18:38 +0200

    Make tests more accurate

--- src/Settings.h
+++ src/Settings.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <array>
 
 namespace Settings
@@ -5,7 +7,7 @@ namespace Settings
     constexpr int32_t MAX_VALUE = INT32_MAX;
     constexpr int32_t MIN_VALUE = INT32_MIN;
 
-    constexpr uint32_t NUMBER_OF_TESTS = 100u;
+    constexpr uint32_t NUMBER_OF_TESTS = 500u;
 
-    constexpr std::array<size_t, 6> TEST_SIZES = {1'000u, 2'000u, 5'000u, 10'000u, 20'000u, 50'000u};
+    constexpr std::array<size_t, 7> TEST_SIZES = {1'000u, 2'000u, 5'000u, 10'000u, 20'000u, 50'000u, 100'000u};
 }

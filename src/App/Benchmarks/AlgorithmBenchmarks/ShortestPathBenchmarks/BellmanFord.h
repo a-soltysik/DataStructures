db740e79134a68a51686e6fb1f2a4e5c6dd2d8cf
Andrzej Soltysik <adzi11235@gmail.com>
2022-05-16 21:43:31 +0200

    Add benchmarks for Shortest Path algorithms

---
+++ src/App/Benchmarks/AlgorithmBenchmarks/ShortestPathBenchmarks/BellmanFord.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace BellmanFordBenchmark
+{
+
+std::string RunBenchmark();
+
+}
---
+++ src/App/Benchmarks/AlgorithmBenchmarks/ShortestPathBenchmarks/DijkstraBenchmark.cpp
@@ -0,0 +1,95 @@
+#include "App/Benchmarks/AlgorithmBenchmarks/ShortestPathBenchmarks/DijkstraBenchmark.h"
+#include "Algorithms/Graphs/DirectedGraphs/DirectedListGraph.h"
+#include "Algorithms/Graphs/DirectedGraphs/DirectedMatrixGraph.h"
+#include "Algorithms/Graphs/GraphGenerator.h"
+#include "Algorithms/ShortestPath/Dijkstra.h"
+#include "App/Settings.h"
+#include "Utils/Timer.h"
+
+namespace DijkstraBenchmark
+{
+
+struct TestCaseResult
+{
+    int64_t directedListGraphTime;
+    int64_t directedMatrixGraphTime;
+};
+
+template<typename T>
+T GenerateGraph(uint32_t order, float density);
+
+TestCaseResult TestCase(uint32_t order, float density);
+
+template<typename T>
+int64_t ConcreteGraphTest(uint32_t order, float density);
+
+Utils::Pair<Graph::Vertex, Graph::Vertex> DrawEdge(const DynamicArray<Graph::Vertex>& vertices);
+
+template<typename T>
+T GenerateGraph(uint32_t order, float density)
+{
+    return Generator::GenerateConnectedGraph<T>({order,
+                                                 density,
+                                                 AlgorithmSettings::MIN_WEIGHT,
+                                                 AlgorithmSettings::MAX_WEIGHT}).value();
+}
+
+std::string RunBenchmark()
+{
+    std::string result = "Benchmark for Dijkstra's algorithm:\n";
+
+    for (const auto& order: AlgorithmSettings::GRAPH_ORDERS)
+    {
+        for (const auto& density : AlgorithmSettings::GraphDensities)
+        {
+            auto[directedListGraphTime, directedMatrixGraphTime] = TestCase(order, density);
+            result.append("Order: " + std::to_string(order) + " Density: " + std::to_string(density) +
+                          ": DirectedListGraph: " + std::to_string(directedListGraphTime) + "ns" +
+                          "; DirectedMatrixGraph: " + std::to_string(directedMatrixGraphTime) + "ns\n");
+        }
+    }
+    return result;
+}
+
+TestCaseResult TestCase(uint32_t order, float density)
+{
+    auto directedListGraphTime = ConcreteGraphTest<DirectedListGraph>(order, density);
+    auto directedMatrixGraphTime = ConcreteGraphTest<DirectedMatrixGraph>(order, density);
+
+    return {directedListGraphTime, directedMatrixGraphTime};
+}
+
+template<typename T>
+int64_t ConcreteGraphTest(uint32_t order, float density)
+{
+    int64_t averageTime = 0u;
+    auto graph = GenerateGraph<T>(order, density);
+    for (uint32_t i = 0u; i < AlgorithmSettings::NUMBER_OF_TESTS; i++)
+    {
+        auto[first, second] = DrawEdge(graph.GetVertices());
+        Utils::Timer timer;
+        timer.Start();
+
+        [[maybe_unused]] volatile auto result = ShortestPath::Dijkstra::FindShortestPath(graph, first, second);
+
+        timer.Stop();
+        averageTime += timer.GetTimeInNanos();
+    }
+    return averageTime / ContainerSettings::NUMBER_OF_TESTS;
+}
+
+Utils::Pair<Graph::Vertex, Graph::Vertex> DrawEdge(const DynamicArray<Graph::Vertex>& vertices)
+{
+    Graph::Vertex first = vertices[static_cast<size_t>(Utils::GetRandomNumber<size_t>(0, vertices.Size() - 1))];
+
+    Graph::Vertex second;
+
+    do
+    {
+        second = vertices[static_cast<size_t>(Utils::GetRandomNumber<size_t>(0, vertices.Size() - 1))];
+    } while (first == second);
+
+    return {first, second};
+}
+
+}
